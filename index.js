446a794b3da5a419bbf34343a6bd119c373a6288
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-10-25 16:50:15 +0300

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 8000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello Wodrld');
 });
 
 server.listen(PORT, () => {
